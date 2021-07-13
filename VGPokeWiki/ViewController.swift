03626a455c38f56bec1d69910bb628bc7f1b7053
7vikram7 <vikramgore2007@gmail.com>
2021-07-13 23:09:20 +0530

    Initial Commit

---
+++ VGPokeWiki/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VGPokeWiki
+//
+//  Created by Vicky's Red Devil on 7/13/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

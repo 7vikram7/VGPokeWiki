1349e0e2538865d8d21230399f589a92448e6c0d
7vikram7 <vikramgore2007@gmail.com>
2021-07-28 21:21:24 +0530

    Unit Tests for Home View Model

--- VGPokeWikiTests/VGPokeWikiTests.swift
+++ VGPokeWikiTests/VGPokeWikiTests.swift
@@ -10,24 +10,30 @@ import XCTest
 
 class VGPokeWikiTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testHomeViewModel_SearchBarShouldChangeTextInRange_ShouldReturnTrue() {
+        //Arrange
+        let sut = HomeViewModel()
+
+        //Act
+        let result = sut.searchBar(UISearchBar(), shouldChangeTextIn: NSRange(), replacementText: "abcdefghijklmnopqurstuvwxyz1234567890")
+
+        //Assert
+        XCTAssertTrue(result, "Only smallcase alphabets and numbers are searchable")
     }
 
+    func testHomeViewModel_SearchBarShouldChangeTextInRange_ShouldReturnFalse() {
+        //Arrange
+        let sut = HomeViewModel()
+
+        //Act
+        let result = sut.searchBar(UISearchBar(), shouldChangeTextIn: NSRange(), replacementText: "~!@#$%^&*()_+{}|:")
+
+        //Assert
+        XCTAssertFalse(result, "Only smallcase alphabets and numbers are searchable")
+    }
 }
